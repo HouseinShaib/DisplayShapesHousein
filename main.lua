7041d0c9f15ab5d74370102261874793af1dd7f2
Housein Shaib <s436317@ocsb.local>
2018-09-26 13:49:07 -0400

    Initial commit

---
+++ main.lua
@@ -0,0 +1,9 @@
+-----------------------------------------------------------------------------------------
+-- Name: Housein Shaib
+-- Course: ICS2O
+-- This program displays different shapes and their names to the tablet screen
+-----------------------------------------------------------------------------------------
+
+-- Create variables
+local areaTextTri
+local areaTextPenta
